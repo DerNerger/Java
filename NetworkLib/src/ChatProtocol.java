ba87dd8d1d7ff15d9b8daae918a748a7cb8da950
felilein <felilein@felilein-P570WM>
2014-05-27 21:35:45 +0200

    bug fixes

--- NetworkLib/src/ChatProtocol.java
+++ NetworkLib/src/ChatProtocol.java
@@ -5,8 +5,8 @@ public class ChatProtocol implements IProtocol{
 	@Override
 	public Packet parsePacket(String msg, String sessionIdSender) {
 		String [] content = msg.split(",");
-		String receiver = content[1].split("=")[1];
-		String message = content[2].split("=")[1];
+		String receiver = content[0].split("=")[1];
+		String message = content[1].split("=")[1];
 		return new ChatMessagePacket(sessionIdSender, receiver, message);
 	}
 
